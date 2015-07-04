4860a16e714d8f83c1bd7686808bcbd1b43562b1
Shaowei <zsw22@vip.qq.com>
2015-07-04 23:36:12 +1000

    搜索框和 autolayout

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 //初始化临时的数据库 全局变量 整个 APP 都可以访问 重启就会清空
 var todos: [TodoModel] = []
+//用来存放搜索数据
+var filteredTodos: [TodoModel] = []
 
 
 func dateFromString(dateStr: String) -> NSDate? {
@@ -21,7 +23,8 @@ func dateFromString(dateStr: String) -> NSDate? {
 
 //引入协议 UITableViewDataSource 同时重写它的函数
 //继承协议 UITableViewDelegate 来删除 Cell
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//继承协议 UISearchDisplayDelegate 用来定义和重写搜索
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchDisplayDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -37,6 +40,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         //激活导航栏左边的 edit 按钮
         navigationItem.leftBarButtonItem = editButtonItem()
+        
+        //启动时隐藏搜索栏 下拉显示
+        var contentOffset = tableView.contentOffset
+        contentOffset.y += searchDisplayController!.searchBar.frame.size.height
+        tableView.contentOffset = contentOffset
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,8 +55,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // UITableViewDataSource 的必要函数重写
     //返回 table 多少行
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        //动态加在多少行
-        return todos.count
+        //如果现在已经在搜索框架 那么返回搜索的个数 否则返回 list 的个数
+        if tableView == searchDisplayController?.searchResultsTableView {
+            return filteredTodos.count
+        } else {
+            //动态加在多少行
+            return todos.count
+        }
     }
     
     // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
@@ -58,8 +71,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         //只执行当前屏幕能显示的 cell 当滚动的时候会释放掉已经消失的 cell 然后加载新的 cell "todoCell" 是 cell 的 id 需要复制到 storyboard 的 cell 中
         let cell = self.tableView.dequeueReusableCellWithIdentifier("todoCell") as UITableViewCell!
         
-        //取出第几行的 TodoModelb
-        let todo = todos[indexPath.row] as TodoModel
+        var todo: TodoModel
+        
+        //判定是搜索与否 按照要求显示
+        if tableView == searchDisplayController?.searchResultsTableView {
+            todo = filteredTodos[indexPath.row] as TodoModel
+        } else {
+            //取出第几行的 TodoModelb
+            todo = todos[indexPath.row] as TodoModel
+        }
+        
         //通过去 storyboard 中给各个控件附加独有的 Tag 来进行绑定(2.0 中强转型 as 要加!)
         let image = cell.viewWithTag(101) as! UIImageView
         let title = cell.viewWithTag(102) as! UILabel
@@ -99,6 +120,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
+    //修改搜索结果的 row 的间距 (因为原始是用的默认高度 而我们之前手动改为80)
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 80
+    }
+    
     //激活 Edit Mode
     override func setEditing(editing: Bool, animated: Bool) {
         super.setEditing(editing, animated: animated)
@@ -117,6 +143,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let todo = todos.removeAtIndex(sourceIndexPath.row)
         //插入到目标行
         todos.insert(todo, atIndex: destinationIndexPath.row)
+    }
+    
+    //搜索功能
+    func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchString searchString: String?) -> Bool{
+        //搜索本地数据库 todos 的参数的 title 包含了 searchString
+        filteredTodos = todos.filter(){
+            $0.title.rangeOfString(searchString!) != nil
+        }
+        return true
     }
     
     
